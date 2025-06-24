016db5cccef5000d74a1004af8c734d89eea382e
Zohaib <120580900+zohaibGit1@users.noreply.github.com>
2025-06-24 16:35:58 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://18.171.209.223:9980/',
+  serverUrl: 'http://13.48.132.236:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_95f458af2e63dfdddb65b4f3e2ccfb04b3455ea9',
+	    'sonar.login': 'sqa_30968c2e80c05c4c30223a509b3f2af60f05d8f1',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
